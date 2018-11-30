22c28bf48832fd154db9bf2fbc5c261dfc451451
Chris Gårdenberg <webmaster@itssimple.se>
2018-11-30 21:45:13 +0100

    add: Adding route for NotDrilled

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,11 +2,13 @@ import * as React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import App from "./App";
 import RescueWindow from "./RescueWindow";
+import NotDrilled from "./NotDrilled";
 
 const Routes = () => (
 	<Router>
 		<Switch>
 			<Route path="/" exact={true} component={App} />
+			<Route path="/NotDrilled" exact={true} component={NotDrilled} />
 			<Route path="/Rescues" exact={true} component={RescueWindow} />
 		</Switch>
 	</Router>
