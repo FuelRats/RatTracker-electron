a1c5ece76ce4809c8d24f562c5b7e816e71dc1e2
NoLifeKing <webmaster@itssimple.se>
2021-03-06 15:54:24 +0100

    fix: Upgrading some packages and removed a method not used

--- src/Overlay.tsx
+++ src/Overlay.tsx
@@ -120,24 +120,6 @@ class Overlay extends React.Component<RatDataProps> {
     }
   }
 
-  // public copySystemName(systemName: string, e: any) {
-  //   var textArea = document.createElement("textarea");
-  //   textArea.value = systemName;
-  //   document.body.appendChild(textArea);
-  //   textArea.focus();
-  //   textArea.select();
-
-  //   try {
-  //     var successful = document.execCommand("copy");
-  //     var msg = successful ? "successful" : "unsuccessful";
-  //     console.log("Fallback: Copying text command was " + msg);
-  //   } catch (err) {
-  //     console.error("Fallback: Oops, unable to copy", err);
-  //   }
-
-  //   document.body.removeChild(textArea);
-  // }
-
   public render() {
     let currentRatName = "";
     let currentRatId = "";
